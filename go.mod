bf0d5f29de21ded86619c9c25fc711e75dc7b0ba
keks <keks@cryptoscope.co>
2019-06-02 00:01:26 +0300

    first international

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/keks/marx
+
+require github.com/hashicorp/go-multierror v1.0.0
---
+++ marx.go
@@ -0,0 +1,37 @@
+package marx // import "github.com/keks/marx"
+
+import (
+	"context"
+	"sync"
+	"github.com/hashicorp/go-multierror"
+)
+
+type Worker func(context.Context) error
+
+func Unite(wfs ...Worker) Worker {
+	return Worker(func(ctx context.Context) error {
+		var (
+			wg sync.WaitGroup
+			l sync.Mutex
+			errs error
+		)
+
+		wg.Add(len(wfs))
+
+		for _, wf := range wfs {
+			go func(wf Worker) {
+				err := wf(ctx)
+				if err != nil {
+					l.Lock()
+					errs = multierror.Append(errs, err)
+					l.Unlock()
+				}
+				wg.Done()
+			}(wf)
+		}
+
+		wg.Wait()
+
+		return errs
+	})
+}
