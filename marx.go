103c9db2827948287a3efebe3e0cbfd00363c9f6
keks <keks@cryptoscope.co>
2019-06-02 00:13:57 +0300

    second international

--- marx.go
+++ marx.go
@@ -20,13 +20,14 @@ func Unite(wfs ...Worker) Worker {
 
 		for _, wf := range wfs {
 			go func(wf Worker) {
+				defer wg.Done()
+
 				err := wf(ctx)
 				if err != nil {
 					l.Lock()
 					errs = multierror.Append(errs, err)
 					l.Unlock()
 				}
-				wg.Done()
 			}(wf)
 		}
 
