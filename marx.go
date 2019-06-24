dbeb37ed24deed812ff299be84e7518bd36b987a
keks <keks@cryptoscope.co>
2019-06-25 01:11:36 +0200

    add unions

--- marx.go
+++ marx.go
@@ -2,6 +2,7 @@ package marx // import "github.com/keks/marx"
 
 import (
 	"context"
+	"errors"
 	"sync"
 
 	"github.com/hashicorp/go-multierror"
@@ -36,4 +37,70 @@ func Unite(wfs ...Worker) Worker {
 
 		return errs
 	})
+}
+
+func NewUnion() (Worker, func(Worker) error) {
+	var (
+		ch     = make(chan Worker)
+		waitCh = make(chan struct{})
+
+		wg   sync.WaitGroup
+		l    sync.Mutex
+		once sync.Once
+		errs error
+	)
+
+	add := func(w Worker) error {
+		l.Lock()
+		defer l.Unlock()
+
+		select {
+		case <-waitCh:
+			return errors.New("union disbanded")
+		default:
+		}
+
+		ch <- w
+		wg.Add(1)
+
+		return nil
+	}
+
+	blockWorker := func(ctx context.Context) error {
+		run := func(ctx context.Context, w Worker) {
+			var err error
+
+			defer func() {
+				l.Lock()
+				if err != nil {
+					errs = multierror.Append(errs, err)
+				}
+				wg.Done()
+				l.Unlock()
+			}()
+
+			err = w(ctx)
+		}
+
+		waiter := func() {
+			wg.Wait()
+			close(waitCh)
+		}
+
+		for {
+			select {
+			case <-waitCh:
+				return errs
+			case w := <-ch:
+				once.Do(func() {
+					go waiter()
+				})
+
+				go run(ctx, w)
+
+			}
+		}
+	}
+
+	return blockWorker, add
 }
